4ed7203477bb656d83c38a79f90901e277e83376
Mickael Blondeau <mickael.blondeau59@gmail.com>
2016-03-13 17:53:32 +0100

    Remplace script on build

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
+var config = require('./config.json');
 var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var header = require('gulp-header');
 var watch = require('gulp-watch');
 var batch = require('gulp-batch');
  
-gulp.task('default', function() {
+gulp.task('default', ['build'], function() {
+	return gulp.src('build/STB3.user.js').pipe(gulp.dest(config.dest));
+});
+
+gulp.task('watch', function() {
+	watch('src/**/*.ts', batch(function(events, done) {
+		gulp.start('default', done);
+	}));
+});
+
+gulp.task('build', function() {
 	return gulp.src('src/**/*.ts')
 		.pipe(ts({
 			noImplicitAny: true,
@@ -22,10 +33,4 @@ gulp.task('default', function() {
 			''
 		].join('\n')))
 		.pipe(gulp.dest('build'));
-});
-
-gulp.task('watch', function() {
-	watch('src/**/*.ts', batch(function(events, done) {
-		gulp.start('default', done);
-	}));
 });
